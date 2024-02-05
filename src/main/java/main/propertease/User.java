4444453da4f4856d302854c179dea1fbcb726ff3
Alexandr Ben Baccar <alexandr.benbaccar001@studenti.uniparthenope.it>
2024-02-05 12:19:48 +0100

    fix for macOS 14

--- src/main/java/main/propertease/User.java
+++ src/main/java/main/propertease/User.java
@@ -1,19 +1,31 @@
 package main.propertease;
 
 public class User {
-    public User(String name, int privileges) {
-        this.name = name;
+    public User(String firstName, String lastName, String username, int privileges) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
         this.privileges = privileges;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUsername() {
+        return username;
     }
 
     public int getPrivileges() {
         return privileges;
     }
 
-    String name;
-    int privileges;
+    private final String firstName;
+    private final String lastName;
+    private final String username;
+    private final int privileges;
 }
