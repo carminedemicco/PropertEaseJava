1be897c582f0fc37b57415ae7d816b5a7abf2fbc
Alexandr Ben Baccar <alexandr.benbaccar001@studenti.uniparthenope.it>
2024-02-26 13:38:12 +0100

    documentation

--- src/main/java/main/propertease/server/StartServer.java
+++ src/main/java/main/propertease/server/StartServer.java
@@ -4,23 +4,20 @@ import main.propertease.server.strategy.AppointmentClientStrategy;
 import main.propertease.server.strategy.HandlerClientStrategy;
 import main.propertease.server.strategy.PosterClientStrategy;
 
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.TimeUnit;
-
 public class StartServer {
     public static void main(String[] args) throws Exception {
-        try (final var forkPool = ForkJoinPool.commonPool()) {
-            final var servers = new Server[]{
-                new Server(1928),
-                new Server(1927),
-                new Server(1926),
-            };
-            forkPool.execute(() -> servers[0].start(AppointmentClientStrategy::new));
-            forkPool.execute(() -> servers[1].start(PosterClientStrategy::new));
-            forkPool.execute(() -> servers[2].start(HandlerClientStrategy::new));
-            while (!forkPool.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS)) {
-                Thread.sleep(1000);
-            }
+        final var servers = new Server[]{
+            new Server(1928),
+            new Server(1927),
+            new Server(1926),
+        };
+        final var threads = new Thread[] {
+            Thread.ofPlatform().start(() -> servers[0].start(AppointmentClientStrategy::new)),
+            Thread.ofPlatform().start(() -> servers[1].start(PosterClientStrategy::new)),
+            Thread.ofPlatform().start(() -> servers[2].start(HandlerClientStrategy::new)),
+        };
+        for (final var thread : threads) {
+            thread.join();
         }
     }
 }
